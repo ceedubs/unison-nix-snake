fe8efd35a43e3205b9b62f8f1ef66cf1d715b2e3
Greg Pfeil <greg@technomadic.org>
2025-05-20 12:30:34 -0600

    Add a Nix formatter and run it

--- flake.nix
+++ flake.nix
@@ -8,48 +8,50 @@
     unison-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils, unison-nix }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    unison-nix,
+  }:
     flake-utils.lib.eachSystem flake-utils.lib.defaultSystems
-      (
-        system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ unison-nix.overlay ];
+    (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [unison-nix.overlay];
+        };
+      in rec {
+        packages = {
+          # A simple example: create an executable from a Unison Share project
+          snake = pkgs.buildUnisonShareProject {
+            pname = "snake";
+            version = "0.0.1";
+            userHandle = "runarorama";
+            projectName = "terminus";
+
+            # The compiledHash is the hash of the compiled Unison code. This
+            # is needed because Nix builds restrict network access unless the
+            # output hash is known ahead of time (which helps with
+            # reproducibility and caching). You won't know it until you run
+            # the derivation for the first time. You can just set this to
+            # `pkgs.lib.fakeHash` and do a `nix build` or `nix run` and copy
+            # the hash labeled `got: `.
+            compiledHash = "sha256-xRjeGswzLLKxPoxHu0hsQzqr7y+1YK0YSXHJSYLb1mo=";
+
+            # A mapping of executable names to Unison functions.
+            executables = {"snake" = "examples.snake.main";};
           };
-        in
-        rec {
-          packages = {
+        };
 
-            # A simple example: create an executable from a Unison Share project
-            snake = pkgs.buildUnisonShareProject {
-              pname = "snake";
-              version = "0.0.1";
-              userHandle = "runarorama";
-              projectName = "terminus";
+        apps = rec {
+          # If your executable name isn't the same as your `pname`, you should also supply `name` argument below.
+          snake = flake-utils.lib.mkApp {drv = packages.snake;};
 
-              # The compiledHash is the hash of the compiled Unison code. This
-              # is needed because Nix builds restrict network access unless the
-              # output hash is known ahead of time (which helps with
-              # reproducibility and caching). You won't know it until you run
-              # the derivation for the first time. You can just set this to
-              # `pkgs.lib.fakeHash` and do a `nix build` or `nix run` and copy
-              # the hash labeled `got: `.
-              compiledHash = "sha256-xRjeGswzLLKxPoxHu0hsQzqr7y+1YK0YSXHJSYLb1mo=";
+          default = snake;
+        };
 
-              # A mapping of executable names to Unison functions.
-              executables = { "snake" = "examples.snake.main"; };
-            };
-
-          };
-
-          apps = rec {
-            # If your executable name isn't the same as your `pname`, you should also supply `name` argument below.
-            snake = flake-utils.lib.mkApp { drv = packages.snake; };
-
-            default = snake;
-          };
-        }
-      );
+        formatter = pkgs.alejandra;
+      }
+    );
 }
-
